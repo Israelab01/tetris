2251a366e4f31555a33f18d33249f0315e8bac0a
AeropuertoTest <israelab001@gmail.com>
2025-02-18 09:58:25 +0100

    html and css done

---
+++ main.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById("tetris");
+
+const ctx = canvas.getContext("2d");
+const pauseBtn = document.getElementById("pauseBtn");
+const scoreElement = document.getElementById("score");
+const highScoreElement = document.getElementById("highScore");
